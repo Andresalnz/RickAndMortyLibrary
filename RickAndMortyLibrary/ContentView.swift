824577e80b7db65616d7fab4c2440258c48ba1d0
Andresalnz <andresaleunz@gmail.com>
2023-10-19 12:30:37 +0200

    Initial Commit

---
+++ RickAndMortyLibrary/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMortyLibrary
+//
+//  Created by Andres Aleu on 19/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMortyLibrary/RickAndMortyLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyLibraryApp.swift
+//  RickAndMortyLibrary
+//
+//  Created by Andres Aleu on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
