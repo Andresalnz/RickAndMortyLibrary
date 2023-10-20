645b2f2e95b867f9cc5f3a2a63c0ae76917be9a7
Andresalnz <andresaleunz@gmail.com>
2023-10-20 14:59:19 +0200

    Add Example JSON

--- RickAndMortyLibrary/Model/Episode/BO/EpisodeModelBO.swift
+++ RickAndMortyLibrary/Model/Episode/BO/EpisodeModelBO.swift
@@ -27,3 +27,20 @@ struct EpisodeModelBO: Codable {
         case created
     }
 }
+
+
+/*
+ {
+   "id": 28,
+   "name": "The Ricklantis Mixup",
+   "air_date": "September 10, 2017",
+   "episode": "S03E07",
+   "characters": [
+     "https://rickandmortyapi.com/api/character/1",
+     "https://rickandmortyapi.com/api/character/2",
+     // ...
+   ],
+   "url": "https://rickandmortyapi.com/api/episode/28",
+   "created": "2017-11-10T12:56:36.618Z"
+ }
+ */
