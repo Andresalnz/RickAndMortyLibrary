273c2633f1c81c2c699a0727accd468761c913d1
Andresalnz <andresaleunz@gmail.com>
2023-10-31 23:25:30 +0100

    Edit View to Section

--- RickAndMortyLibrary/UseCases/DetailCharacter/DetailCharacterView.swift
+++ RickAndMortyLibrary/UseCases/DetailCharacter/DetailCharacterView.swift
@@ -14,50 +14,68 @@ struct DetailCharacterView: View {
     @StateObject var viewModel: DetailCharacterViewModel
     
     var body: some View {
-        ScrollView {
-            VStack {
-                //MARK: - Imagen y Nombre
-                Text(character.name ?? Constants.noText)
-                    .bold()
-                    .font(.title)
-                    .frame(maxWidth: Constants.maxWidthTitlesDetail, alignment: .leading)
-                
+        Text("Character Detail")
+            .font(.title)
+        List {
+            //MARK: - Seccion de la imagen
+            Section("Image") {
                 if let image = character.image {
-                   AsyncImageView(urlImage: image)
-                        .frame(width: 340, height: 380)
-                        .clipShape(RoundedRectangle(cornerSize: CGSize(width: Constants.sizeCornerRadius, height: Constants.sizeCornerRadius)))
+                    AsyncImageView(urlImage: image)
+                        .scaledToFill()
+                        .listRowBackground(RoundedRectangle(cornerRadius: 8)
+                            .background(.clear)
+                            .foregroundColor(.clear)
+                            .padding(.vertical)
+                        )
                         .shadow(radius: 8)
                 } else {
                     Image(systemName: "person.fill")
                 }
                 
-                //MARK: - INFO
-                Text("info")
-                    .bold()
-                    .textCase(.uppercase)
-                    .font(.title)
-                    .frame(maxWidth: Constants.maxWidthTitlesDetail, alignment: .leading)
-                InfoDetailCharacterView(character: character)
-                
-                //MARK: - Episodios
-                Text("episodes")
-                    .bold()
-                    .textCase(.uppercase)
-                    .font(.title)
-                    .frame(maxWidth: Constants.maxWidthTitlesDetail, alignment: .leading)
-                
-                ForEach(viewModel.allEpisodes, id: \.id) { episode in
-                    Text(episode.name ?? Constants.noText)
-                        .foregroundStyle(.red)
-                        .font(.body)
-                        .padding(.top)
-                        .frame(maxWidth: Constants.maxWidthTitlesDetail, alignment: .leading)
-                }
             }
-            .onAppear {
-                viewModel.loadUI()
+            //MARK: - Seccion de la informacion
+            Section("Information") {
+                InfoDetailCharacterRowView(icon: "circle.fill", label: Text("Status"), value: Text(character.status?.rawValue ?? "").foregroundColor(character.status == .alive ? .green : .black))
+                InfoDetailCharacterRowView(icon: "figure.arms.open", label: Text("Specie"), value: Text(character.species?.rawValue ?? Constants.noText))
+                if character.type != "" {
+                    InfoDetailCharacterRowView(icon: "figure.arms.open", label: Text("Type"), value: Text(character.type ?? Constants.noText))
+                }
+                InfoDetailCharacterRowView(icon: "person.fill", label: Text("Gender"), value: Text(character.gender?.rawValue ?? Constants.noText))
+                InfoDetailCharacterRowView(icon: "globe.asia.australia.fill", label: Text("Origin"), value: Text(character.origin?.name ?? Constants.noText))
+                InfoDetailCharacterRowView(icon: "location.fill", label: Text("Location"), value: Text(character.location?.name ?? Constants.noText))
+            }
+            //MARK: - Seccion de los episodios
+            Section("Episodes") {
+                ForEach(viewModel.allEpisodes, id: \.id) { episode in
+                    VStack {
+                        Text(episode.name ?? Constants.noText)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundStyle(.white)
+                        HStack {
+                            Text("September 10, 2017")
+                                .foregroundStyle(.white)
+                            Spacer()
+                            Text(episode.episode ?? "")
+                                .foregroundStyle(.white)
+                        }
+                        .padding(.top)
+                    }
+                }
+                .listRowSeparator(.hidden)
+                .listRowBackground(RoundedRectangle(cornerRadius: 8)
+                    .background(.clear)
+                    .foregroundStyle(.gray)
+                    .opacity(0.8)
+                    .padding(.bottom, 5)
+                )
             }
         }
+        .onAppear {
+            viewModel.loadUI()
+        }
+        .listStyle(.insetGrouped)
+        .navigationBarBackButtonHidden(false)
+        .navigationBarHidden(true)
     }
 }
 
