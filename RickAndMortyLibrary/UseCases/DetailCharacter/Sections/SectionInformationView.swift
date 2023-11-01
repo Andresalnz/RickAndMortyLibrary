92c9e3656b329283d26da7a4590832cbaba98136
Andresalnz <andresaleunz@gmail.com>
2023-11-01 19:13:14 +0100

    Edit Icon

--- RickAndMortyLibrary/UseCases/DetailCharacter/Sections/SectionInformationView.swift
+++ RickAndMortyLibrary/UseCases/DetailCharacter/Sections/SectionInformationView.swift
@@ -14,9 +14,9 @@ struct SectionInformationView: View {
     var body: some View {
         Section("Information") {
             InfoDetailCharacterRowView(icon: "circle.fill", label: Text("Status"), value: Text(character.status?.rawValue ?? "").foregroundColor(character.status == .alive ? .green : .black))
-            InfoDetailCharacterRowView(icon: "figure.arms.open", label: Text("Specie"), value: Text(character.species?.rawValue ?? Constants.noText))
+            InfoDetailCharacterRowView(icon: "person.circle", label: Text("Specie"), value: Text(character.species?.rawValue ?? Constants.noText))
             if character.type != "" {
-                InfoDetailCharacterRowView(icon: "figure.arms.open", label: Text("Type"), value: Text(character.type ?? Constants.noText))
+                InfoDetailCharacterRowView(icon: "person.circle", label: Text("Type"), value: Text(character.type ?? Constants.noText))
             }
             InfoDetailCharacterRowView(icon: "person.fill", label: Text("Gender"), value: Text(character.gender?.rawValue ?? Constants.noText))
             InfoDetailCharacterRowView(icon: "globe.asia.australia.fill", label: Text("Origin"), value: Text(character.origin?.name ?? Constants.noText))
