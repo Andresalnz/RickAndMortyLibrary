5fa5954a42d4448e5552474a38b5061d501dbd43
Andresalnz <andresaleunz@gmail.com>
2024-01-23 22:28:10 +0100

    edit options

--- RickAndMortyLibrary/Utils/Util.swift
+++ RickAndMortyLibrary/Utils/Util.swift
@@ -12,16 +12,16 @@ final class Util {
     //MARK: - metodo para crear la URL
     enum Services {
         case allCharacters
-        case episodes
-        case locations
+        case allEpisodes
+        case allLocations
         
         func shapeURL() -> URL? {
             switch self {
                 case .allCharacters:
                     return URL(string: "\(Constants.uri + Constants.characters)")
-                case .episodes:
+                case .allEpisodes:
                     return URL(string: "\(Constants.uri + Constants.episodes)")
-                case .locations:
+                case .allLocations:
                     return URL(string: "\(Constants.uri + Constants.locations)")
             }
         }
