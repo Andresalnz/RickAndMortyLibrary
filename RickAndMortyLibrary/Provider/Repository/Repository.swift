21ea01349bfe9c8d53ea0a9506696ea32c365cbd
Andresalnz <andresaleunz@gmail.com>
2024-01-23 22:37:28 +0100

    Add new repository and add interactor

---
+++ RickAndMortyLibrary/Provider/Repository/Repository.swift
@@ -0,0 +1,21 @@
+//
+//  CharacterRepository.swift
+//  RickAndMortyLibrary
+//
+//  Created by Andres Aleu on 15/11/23.
+//
+
+import Foundation
+
+struct Repository {
+    func getJSON<T>(url: URL?, type: T.Type) async throws -> T where T: Codable {
+        guard let url = url else { throw ErrorHandler.invalidUrl }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let rickAndMortyModel = try JSONDecoder().decode(type, from: data)
+            return rickAndMortyModel
+        } catch {
+            throw ErrorHandler.requestCharactersInvalid
+        }
+    }
+}
