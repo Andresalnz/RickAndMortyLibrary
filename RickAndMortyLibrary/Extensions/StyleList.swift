f0109f3f9622dd58fbdd5d24763f045ff63d5e68
Andresalnz <andresaleunz@gmail.com>
2024-01-24 12:14:16 +0100

    add custom style List

--- RickAndMortyLibrary/Extensions/StyleList.swift
+++ RickAndMortyLibrary/Extensions/StyleList.swift
@@ -5,4 +5,19 @@
 //  Created by Andres Aleu on 24/1/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct StyleList: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(.bottom, 10)
+            .listRowSeparator(.hidden)
+            .listRowBackground(RoundedRectangle(cornerRadius: Constants.sizeCornerRadius)
+                .background(.clear)
+                .foregroundStyle(.white
+                                )
+                .opacity(0.8)
+                .padding(.bottom, 10)
+            )
+    }
+}
