d9ff7d177e7176af9ff790d0aaf3470751fde8fc
Andresalnz <andresaleunz@gmail.com>
2024-02-01 19:15:08 +0100

    Edit favourite view with @ViewBuilder

--- RickAndMortyLibrary/UseCases/Favourites/FavouritesView.swift
+++ RickAndMortyLibrary/UseCases/Favourites/FavouritesView.swift
@@ -9,20 +9,58 @@ import SwiftUI
 
 struct FavouritesView: View {
     @StateObject var viewModel: FavouritesViewModel
+    let type: TypeViewList
     
     var body: some View {
-        List {
-            ForEach(viewModel.info ,id: \.id) { info in
+        VStack {
+            HStack(alignment: .center, spacing: 50) {
+                ButtonFilterView(title: "Locations") {
+                    viewModel.filterEpisodes()
+                }
                 
-                Text(info.name ?? "no name")
+                ButtonFilterView(title: "Characters") {
+                    viewModel.filterCharacters()
+                }
+                
+                ButtonFilterView(title: "Episodes") {
+                    viewModel.filterLocation()
+                }
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.top, 30)
+            
+            List {
+                FavouriteContent
+                    .modifier(StyleList())
+            }
+            .onAppear {
+                viewModel.loadUI()
             }
         }
-        .onAppear {
-            viewModel.loadUI()
+    }
+    
+    
+    @ViewBuilder
+    private var FavouriteContent: some View {
+        switch viewModel.ischaracter {
+            case .characters:
+                ForEach(viewModel.characters ,id: \.id) { info in
+                    CharacterFavouriteView(model: info)
+                }
+            case .locations:
+                ForEach(viewModel.locations, id: \.id) { info in
+                    EpisodeLocationFavourite(model: info)
+                    
+                }
+            case .episodes:
+                ForEach(viewModel.episodes ,id: \.id) { info in
+                    EpisodeLocationFavourite(model: info)
+                }
+                
         }
     }
 }
 
 #Preview {
-    FavouritesView(viewModel: FavouritesViewModel())
+    FavouritesView(viewModel: FavouritesViewModel(), type: .characters)
 }
