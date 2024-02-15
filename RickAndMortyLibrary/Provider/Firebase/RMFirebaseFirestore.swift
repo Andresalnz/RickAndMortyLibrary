bc76c50ec523d8416f8096b296cc6901f55cff23
Andresalnz <andresaleunz@gmail.com>
2024-02-15 20:07:55 +0100

    Add method for get documents

--- RickAndMortyLibrary/Provider/Firebase/RMFirebaseFirestore.swift
+++ RickAndMortyLibrary/Provider/Firebase/RMFirebaseFirestore.swift
@@ -62,4 +62,14 @@ struct RMFirebaseFirestore {
             throw ErrorHandler.FirestoreNotSaveDocument
         }
     }
+    
+    //MARK: - Obtener colecciones
+    func getDocuments<T>(collection: String, type: T.Type) async throws -> [T] where T: Codable {
+        do {
+            let querySnapchot = try await database.collection(collection).getDocuments()
+            return querySnapchot.documents.compactMap { try? $0.data(as: type) }
+        } catch {
+            throw ErrorHandler.FirestoreNotGetDocuments
+        }
+    }
 }
